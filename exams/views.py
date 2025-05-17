baccc00d67eb7b4f3066281ed7a89799ea983324
SpodikR <spodromas@gmail.com>
2025-05-17 14:35:51 +0300

    Додав представлення (views)

--- exams/views.py
+++ exams/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+# exams/views.py
+from django.shortcuts import render, get_object_or_404
+from .models import Subject, Ticket, Question
 
-# Create your views here.
+def subject_list_view(request):
+    subjects = Subject.objects.all().prefetch_related('tickets') # prefetch_related для оптимізації
+    context = {
+        'subjects': subjects
+    }
+    return render(request, 'exams/subject_list.html', context)
+
+def ticket_list_view(request, subject_id):
+    subject = get_object_or_404(Subject, id=subject_id)
+    tickets = Ticket.objects.filter(subject=subject).prefetch_related('questions') # prefetch_related для оптимізації
+    context = {
+        'subject': subject,
+        'tickets': tickets
+    }
+    return render(request, 'exams/ticket_list.html', context)
+
+def ticket_detail_view(request, ticket_id):
+    ticket = get_object_or_404(Ticket.objects.select_related('subject').prefetch_related('questions'), id=ticket_id)
+    # select_related('subject') для оптимізації доступу до subject.name
+    # prefetch_related('questions') для оптимізації доступу до ticket.questions.all
+    context = {
+        'ticket': ticket
+    }
+    return render(request, 'exams/ticket_detail.html', context)
